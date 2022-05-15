c5613c1c3647c564ca05b2752361bfc540e55027
KimSeongtae <seongtae07@gmail.com>
2022-05-15 22:05:20 +0900

    stone2.py

---
+++ games/stone2.py
@@ -0,0 +1,125 @@
+from tkinter import  *
+
+class Cell(Canvas):
+    def __init__(self, parent, row, col, width=20, height=20):
+        Canvas.__init__(self, parent, width=width, height=height, bg="blue", borderwidth=2)
+        self.color = 'white'
+        self.row = row
+        self.col = col
+        self.create_oval(4, 4, 20, 20, fill="white", tags="oval")
+        self.bind("<Button-1>", lambda t, r=row, c=col: pressed(r,c))
+
+    def clicked(self):
+        if self.color != 'white': 
+            return False
+        self.delete("oval") 
+
+        if turn:
+            self.color = 'red'
+        else:
+            self.color = 'yellow'   
+        self.create_oval(4, 4, 20, 20, fill=self.color, tags="oval")
+        return True
+
+    def refresh(self):
+        self.color = 'white'
+        self["bg"] = 'blue'
+        self.delete("oval")
+        self.create_oval(4, 4, 20, 20, fill='white', tags='oval')
+
+def check():
+    for r in range(6):
+        for c in range(4):
+            color = cells[r][c].color
+            if color != 'white' and color == cells[r][c + 1].color and color == cells[r][c + 2].color and color == cells[r][c + 3].color:
+                for i in range(4):
+                    cells[r][c+i]['bg'] = color
+                return color
+
+    for r in range(3):
+        for c in range(7):
+            color = cells[r][c].color
+            if color != 'white' and color == cells[r+1][c].color and color == cells[r + 2][c].color and color == cells[r + 3][c].color:
+                for i in range(4):
+                    cells[r+i][c]['bg'] = color
+                return color
+
+    for r in range(3):		# 우하단 대각
+        for c in range(4):
+            color = cells[r][c].color
+            if color != 'white' and color == cells[r + 1][c + 1].color and color == cells[r + 2][c + 2].color and color == cells[r + 3][c + 3].color:
+                for i in range(4):
+                    cells[r + i][c + i]['bg'] = color
+                return color
+
+    for r in range(3):
+        for c in range(3, 7, 1):
+            color = cells[r][c].color
+            if color != 'white' and color == cells[r + 1][c - 1].color and color == cells[r + 2][c - 2].color and color == cells[r + 3][c - 3].color:
+                for i in range(4):
+                    cells[r + i][c - i]['bg'] = color
+                return color
+
+    for r in range(6):
+        for c in range(7):
+            if cells[r][c].color == 'white':
+                return 'white'
+
+    return '@'
+
+def pressed(row, col):
+    global done, turn
+    
+    if done or cells[row][col].color != 'white':
+        return
+    
+    for r in range(5, -1, -1):
+        if cells[r][col].color == 'white':
+            cells[r][col].clicked()
+            turn = not turn
+
+            color = check()
+
+            text = ''
+            if color == '@':
+                text = '비김'
+                done = True
+            elif color != 'white':
+                text = color + '승리'
+                done = True
+            else:
+                text = '다시시작'
+            explaneLabel['text'] = text
+
+            break
+
+def restart():
+    global turn, done
+
+    turn = True
+    done = False
+    explaneLabel['text'] = '다시시작'
+    for r in range(6):
+        for c in range(7):
+            cells[r][c].refresh()
+
+turn = True
+done = False
+
+window = Tk()
+window.title("사목게임")
+
+frame = Frame(window)
+frame.pack()
+
+cells = []
+for r in range(6):
+    cells.append([])
+    for c in range(7): 
+        cells[r].append(Cell(frame, r, c, 20, 20))
+        cells[r][c].grid(row=r, column=c)
+
+explaneLabel = Button(window, text="다시시작", command=restart)
+explaneLabel.pack()
+
+window.mainloop()
